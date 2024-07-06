9ed0028e8da1628963c6900e2d6bd5790b1200f0
ncn1 <help2921@gmail.com>
2024-07-06 22:52:28 +0300

    Incercare de ajustare la directie

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AdvancedTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AdvancedTeleOp.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import static java.lang.Thread.sleep;
+
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -18,6 +20,8 @@ public class AdvancedTeleOp extends OpMode {
 
     private int previousEncoderPosition;
     private long previousTime;
+
+    private boolean StrngCalEnter = false;
 
     @Override
     public void init() {
@@ -42,80 +46,92 @@ public class AdvancedTeleOp extends OpMode {
 
     @Override
     public void loop() {
-        double lt = gamepad1.left_trigger;
-        double rt = gamepad1.right_trigger;
-        double joy_axis = gamepad1.left_stick_x;
-        boolean claw = gamepad1.circle;
-        boolean hand_break = gamepad1.square;
-
-        // Call the movement logic function
-        MovementOutput output = movementLogic(lt, rt, joy_axis);
-
-        if (!output.isValid) {
-            telemetry.addData("Error", "Invalid inputs");
-        } else if (!hand_break){
-            rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            // Set motor power
-            leftMotor.setPower(output.leftMotor);
-            rightMotor.setPower(output.rightMotor);
-
-            int targetTurn = output.turnAngle;
-            //int currentTurn = turnMotor.getCurrentPosition();
-            //int coeff = targetTurn / Math.max(Math.abs(targetTurn), 1);
-            turnMotor.setTargetPosition(targetTurn);  //(Math.abs(targetTurn) + 50) * coeff);
+        if (!StrngCalEnter){
+            turnMotor.setPower(0.5);
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            turnMotor.setTargetPosition(turnMotor.getCurrentPosition() - 450);  //(Math.abs(targetTurn) + 50) * coeff);
             turnMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             turnMotor.setPower(1.0);
 
-            enter = 0;
+            turnMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            turnMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-            // Telemetry data for debugging
-            telemetry.addData("Left Motor Power", leftMotor.getPower());
-            telemetry.addData("Right Motor Power", rightMotor.getPower());
-            telemetry.addData("Turn Motor Position", output.turnAngle);
-            telemetry.addData("Real turn angle positio n:", turnMotor.getCurrentPosition());
-            double turnMotorSpeed = getMotorSpeed(rightMotor);
-            telemetry.addData("Right Motor Speed", turnMotorSpeed);
-            telemetry.addData("x", gamepad1.cross);
+            StrngCalEnter = true;
         }
         else {
-            leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-            rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            double lt = gamepad1.left_trigger;
+            double rt = gamepad1.right_trigger;
+            double joy_axis = gamepad1.left_stick_x;
+            boolean claw = gamepad1.circle;
+            boolean hand_break = gamepad1.square;
 
-            leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            leftMotor.setPower(0);
+            // Call the movement logic function
+            MovementOutput output = movementLogic(lt, rt, joy_axis);
 
-            telemetry.addData("Left Motor Power", leftMotor.getPower());
-            telemetry.addData("Right Motor Power", rightMotor.getPower());
-            telemetry.addData("Turn Motor Position", output.turnAngle);
-            telemetry.addData("Real turn angle positio n:", turnMotor.getCurrentPosition());
-            double turnMotorSpeed = getMotorSpeed(rightMotor);
-            telemetry.addData("Right Motor Speed", turnMotorSpeed);
-            telemetry.addData("x", gamepad1.cross);
+            if (!output.isValid) {
+                telemetry.addData("Error", "Invalid inputs");
+            } else if (!hand_break) {
+                rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                // Set motor power
+                leftMotor.setPower(output.leftMotor);
+                rightMotor.setPower(output.rightMotor);
 
-            if (enter > 0) {
-                rightMotor.setTargetPosition(targetBreak);
-                rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                if (Math.abs(rightMotor.getCurrentPosition()) - 50 > Math.abs(targetBreak)) {
-                    rightMotor.setPower(0.69);
+                int targetTurn = output.turnAngle;
+                //int currentTurn = turnMotor.getCurrentPosition();
+                //int coeff = targetTurn / Math.max(Math.abs(targetTurn), 1);
+                turnMotor.setTargetPosition(targetTurn);  //(Math.abs(targetTurn) + 50) * coeff);
+                turnMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                turnMotor.setPower(1.0);
+
+                enter = 0;
+
+                // Telemetry data for debugging
+                telemetry.addData("Left Motor Power", leftMotor.getPower());
+                telemetry.addData("Right Motor Power", rightMotor.getPower());
+                telemetry.addData("Turn Motor Position", output.turnAngle);
+                telemetry.addData("Real turn angle position n:", turnMotor.getCurrentPosition());
+                double turnMotorSpeed = getMotorSpeed(rightMotor);
+                telemetry.addData("Right Motor Speed", turnMotorSpeed);
+            } else {
+                leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+                leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                leftMotor.setPower(0);
+
+                telemetry.addData("Left Motor Power", leftMotor.getPower());
+                telemetry.addData("Right Motor Power", rightMotor.getPower());
+                telemetry.addData("Turn Motor Position", output.turnAngle);
+                telemetry.addData("Real turn angle position n:", turnMotor.getCurrentPosition());
+                double turnMotorSpeed = getMotorSpeed(rightMotor);
+                telemetry.addData("Right Motor Speed", turnMotorSpeed);
+
+                if (enter > 0) {
+                    rightMotor.setTargetPosition(targetBreak);
+                    rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    if (Math.abs(rightMotor.getCurrentPosition()) - 50 > Math.abs(targetBreak)) {
+                        rightMotor.setPower(0.69);
+                    } else {
+                        rightMotor.setPower(0.15);
+                    }
+                } else {
+                    targetBreak = rightMotor.getCurrentPosition();
                 }
-                else {
-                    rightMotor.setPower(0.15);
-                }
-            }
-            else {
-                targetBreak = rightMotor.getCurrentPosition();
-            }
 
-            enter ++;
-        }
-        telemetry.update();
-        if (claw) {
-            clawMotor.setPosition(0.4);
-        }
-        else {
-            clawMotor.setPosition(1.0);
+                enter++;
+            }
+            telemetry.update();
+            if (claw) {
+                clawMotor.setPosition(0.35);
+            } else {
+                clawMotor.setPosition(1.0);
+            }
         }
     }
 
@@ -137,12 +153,20 @@ public class AdvancedTeleOp extends OpMode {
         MovementOutput output = new MovementOutput();
 
         double max_angle;
+        double max_pow;
 
         if (gamepad1.cross) {
             max_angle = 450;
         }
         else {
             max_angle = 300;
+        }
+
+        if (gamepad1.triangle) {
+            max_pow = -0.4;
+        }
+        else {
+            max_pow = -1.0;
         }
 
         if (!(lt >= -1 && lt <= 1 && rt >= -1 && rt <= 1 && joy_axis >= -1 && joy_axis <= 1)) {
@@ -153,8 +177,8 @@ public class AdvancedTeleOp extends OpMode {
         output.isValid = true;
         double speed = rt - lt;
         output.turnAngle = (int) (joy_axis * -max_angle);
-        double l_motor =  speed * -1;
-        double r_motor =  speed * -1;
+        double l_motor =  speed * max_pow;
+        double r_motor =  speed * max_pow;
 
         output.leftMotor = Math.max(-1, Math.min(l_motor, 1));
         output.rightMotor = Math.max(-1, Math.min(r_motor, 1));
